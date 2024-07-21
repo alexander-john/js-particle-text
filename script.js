f2aa246fe844263b7cd54f8a340527f0aa0d56c0
Alexander John <96aljohn@gmail.com>
2024-07-20 20:58:55 -0700

    update-js

--- script.js
+++ script.js
@@ -3,4 +3,30 @@ window.addEventListener('load', function() {
     const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'red';
+    ctx.beginPath();
+    ctx.moveTo(canvas.width/2, 0);
+    ctx.lineTo(canvas.width/2, canvas.height);
+    ctx.stroke();
+
+    ctx.strokeStyle = 'green';
+    ctx.beginPath();
+    ctx.moveTo(0, canvas.height/2);
+    ctx.lineTo(canvas.width, canvas.height/2);
+    ctx.stroke();
+
+    const text = 'Hello';
+    const textX = canvas.width/2;
+    const textY = canvas.height/2;
+    console.log(ctx);
+    ctx.fillStyle = 'yellow';
+    ctx.strokeStyle = 'white';
+
+    ctx.font = '80px Helvetica';
+    ctx.textAlign = 'end';
+    ctx.fillText(text, textX, textY);
+    ctx.strokeText(text, textX, textY);
+
 })
